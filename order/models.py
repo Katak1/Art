048e1aafa5564d41aa7b600de834e537cb5540f9
manas <kaiymovmanas05@gmail.com>
2023-01-26 16:26:26 +0600

    added order

---
+++ order/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from art_models.models import ArtGallery
+
+User = get_user_model()
+
+
+class Order(models.Model):
+    STATUS_CHOISES = (
+        ('open', 'Открыт'),
+        ('processing', 'В процессе'),
+        ('closed', 'Закрыт')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='users')
+    art = models.ForeignKey(ArtGallery, on_delete=models.SET_NULL, related_name='arts')
+    status = models.CharField(max_length=10, choices=STATUS_CHOISES, default='open')
+    order_date = models.DateTimeField(auto_now=True)
